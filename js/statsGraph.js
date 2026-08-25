e574f66fe110b98842fdcab2b054d74063e96030
Bill Mead <bill@meaddesign.net>
2026-08-25 15:04:35 -0700

    updated graphics

--- js/statsGraph.js
+++ js/statsGraph.js
@@ -68,7 +68,7 @@ export function renderStatsGraph(container, { averageScore = 0, winPercent = 0 }
         <div class="infograph-container">
             <div class="wedge">${createWedgeSVG(topAngle)}</div>
             <div class="wedge">${createWedgeSVG(bottomAngle, { clockwise: false, fill: 'red' })}</div>
-            <img src="images/circleGraphic.png" alt="game statistics graphic" class="graphic">
+            <img src="images/circleGraphic2.png" alt="game statistics graphic" class="graphic">
 
             <div class="wedge">
                 <svg width="100%" height="100%" viewBox="0 0 100 100">
